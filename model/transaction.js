e665cedec6246ef34b5887405e654d26ac4621a7
Josh Wickster <joshwickster@gmail.com>
2020-12-17 18:05:01 -0700

    added transaction model

---
+++ model/transaction.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Transaction extends Model{}
+
+Transaction.init({
+      tCost: {
+        type: DataTypes.DECIMAL(10,30),
+        allow_null: false,
+      },
+      tPortionSize: {
+        type: DataTypes.DECIMAL(1,30),
+        allow_null: false,
+      },
+      tOrderNumber: {
+        type: DataTypes.INTEGER(5),
+        allow_null: false,
+        primaryKey: true,
+      },
+      
+      
+    },
+    {
+      sequelize,
+      timestamp: true,
+      underscored: true,
+      modelName: 'buyer',
+    },
+});
