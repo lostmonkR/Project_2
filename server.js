fb1a09de9603dfff4fc1a064bc924fb694f9fcde
Josh Wickster <joshwickster@gmail.com>
2020-12-17 17:58:19 -0700

    added config and connection.js

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var shortId = require('shortid');
+
+
 
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 
 var port = process.env.PORT || 8080;
-var router = express.Router();
+const express = require('express');
+const sequelize = require('./config/connection');
+
+const Book = require('./models/Book');
+
+const router = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 
 
 //CORS https://stackoverflow.com/questions/32500073/request-header-field-access-control-allow-headers-is-not-allowed-by-itself-in-pr
@@ -21,14 +32,12 @@ router.use(function(req, res, next) {
   next()
 })
 
-router.use(function(req, res, next){
-  console.log('\nReceived', {
-    url: req.originalUrl,
-    body: req.body,
-    query: req.query,
-  })
-  next();
-})
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
+
 
 
 
