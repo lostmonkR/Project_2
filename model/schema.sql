11c90eddc91340584a98deb76eb9336d5ba27561
Josh Wickster <joshwickster@gmail.com>
2020-12-17 17:07:50 -0700

    refactored CORS support

--- model/schema.sql
+++ model/schema.sql
@@ -1,42 +1,28 @@
--- Drops  --
-DROP DATABASE IF EXISTS FoodHub_db;
--- Creates  --
-CREATE DATABASE FoodHub_db;
+DROP DATABASE IF EXISTS user_db;
+CREATE DATABASE user_db;
 
-
-USE FoodHub_db;
-
--- TODO Create Tables --
 
 CREATE TABLE buyer (
-    bFname VARCHAR(30) NOT NULL,
-    bLname VARCHAR(30) NOT NULL,
-    bPhoneNumber INTEGER(7) NOT NULL,
-    bAddress VARCHAR(200) NOT NULL,
-    bCity VARCHAR(20) NOT NULL,
-    bState VARCHAR(20) NOT NULL,
-    bneedsDelivered BOOLEAN NOT NULL
+        bFname VARCHAR(30) NOT NULL,
+        bLname VARCHAR(30) NOT NULL,
+        bPhoneNumber INTEGER(7) NOT NULL,
+        bAddress VARCHAR(200) NOT NULL,
+        bCity VARCHAR(20) NOT NULL,
+        bState VARCHAR(20) NOT NULL,
+        bneedsDelivered BOOLEAN NOT NULL,
+        PRIMARY KEY (bPhoneNumber)
 
 );
 
 Create TABLE seller(
 
-    sFname VARCHAR(30) NOT NULL,
-    sLname VARCHAR(30) NOT NULL,
-    sPhoneNumber INTEGER(7) NOT NULL,
-    sAddress VARCHAR(200) NOT NULL,
-    sCity VARCHAR(20) NOT NULL,
-    sState VARCHAR(20) NOT NULL,
-    sCanDeliver BOOLEAN NOT NULL,
-    cost DECIMAL(10,2)
+        sFname VARCHAR(30) NOT NULL,
+        sLname VARCHAR(30) NOT NULL,
+        sPhoneNumber INTEGER(7) NOT NULL,
+        sAddress VARCHAR(200) NOT NULL,
+        sCity VARCHAR(20) NOT NULL,
+        sState VARCHAR(20) NOT NULL,
+        sCanDeliver BOOLEAN NOT NULL,
+        PRIMARY KEY (sPhoneNumber)
 
 );
-
-Create Table food_product(
-    f_Name VARCHAR(30) NOT NULL,
-    time_made VARCHAR(10) NOT NULL,
-    f_portion_size DECIMAL(10,2) NOT NULL,
-    f_calories INTEGER(1000) NOT NULL,
-    cost DECIMAL(10,2)
-)
-
