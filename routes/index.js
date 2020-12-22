5bb63471a52d6aeb3d0a8961c5f8944988d4c9e7
Josh Wickster <joshwickster@gmail.com>
2020-12-21 19:33:04 -0700

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
+
+const sellerInformation = require('./sellerRoutes.js');
+const buyerInformation = require('./buyerRoutes.js');
+const transactionInformation = require('transactionRoutes.js');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/transactionRoutes', transactionInformation);
+router.use('/buyerRoutes', buyerInformation);
+router.use('/sellerRoutes', sellerInformation)
 
 module.exports = router;
--- utils/withAuth.js
+++ utils/withAuth.js
@@ -1,4 +1,4 @@
-const withAuth = function(req, res, next) {
+const withAuth = (req, res, next) => {
   if(!req.session.bId){
     res.redirect('/index');
   }else{
